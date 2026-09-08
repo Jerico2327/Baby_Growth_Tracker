b86e1f13618e63dbb8e10ca0f3e3a29772c3cee0
Jerico2327 <magcalejerico62@gmail.com>
2026-09-08 18:34:09 +0800

    Add comments, javascript in the submit button, Utilized zscore data file for AI's evaluation

--- bmi.py
+++ bmi.py
@@ -1,25 +1,35 @@
 import os
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField, DateField, FloatField
+from wtforms import SelectField, SubmitField, DateField, FloatField
 from wtforms.validators import DataRequired
-import pandas as pd
 from dateutil.relativedelta import relativedelta
 from datetime import datetime as dt
 from google import genai
 
 
 class UserInput(FlaskForm):
-    gender = SelectField("Gender",
-                         choices=[("", ""), ("boy", "Male"), ("girl", "Female")],
+
+    """ Generates the form to be filled by the user """
+
+    gender = SelectField("",
+                         choices=[("", "Gender"), ("boy", "Boy"), ("girl", "Girl")],
                          validators=[DataRequired()])
-    birthdate = DateField("Birthdate", validators=[DataRequired()])
-    weight = FloatField("Weight (kg)", validators=[DataRequired(message="Numeric input is required.")])
-    height = FloatField("Height (m)", validators=[DataRequired(message="Numeric input is required.")])
-    submit = SubmitField("Calculate BMI")
+    birthdate = DateField("",
+                          render_kw={"placeholder": "dd/mm/yyyy"},
+                          validators=[DataRequired()])
+    weight = FloatField("",
+                        render_kw={"placeholder": "Weight (kg)"},
+                        validators=[DataRequired(message="Numeric input is required.")])
+    height = FloatField("",
+                        render_kw={"placeholder": "Height (m)"},
+                        validators=[DataRequired(message="Numeric input is required.")])
+    submit = SubmitField("Calculate", render_kw={"class": "w-75"})
 
 
 class Calculate:
+
+    """ Performs the calculations needed to get the BMI. """
 
     @staticmethod
     def bmi(weight, height):
@@ -40,15 +50,24 @@ class Calculate:
 
 class BMIReference:
 
-    def __init__(self, df):
-        self.df = df
+    """
+        Serves as the reference for WHO data.
+        This class uses the Dataframes downloaded from the WHO website, mainly the percentile and z-score.
+        https://www.who.int/toolkits/child-growth-standards/standards/body-mass-index-for-age-bmi-for-age
+    """
+
+    def __init__(self, percentile_df, zscore_df):
+        self.percentile_df = percentile_df
+        self.zscore_df = zscore_df
 
     def get_percentile(self, age, bmi):
+
+        """ Returns the boundaries where the calculated BMI lies. """
 
         lower_percentile = None
         upper_percentile = None
         limits = []
-        new_df = self.df.copy().set_index("Month")
+        new_df = self.percentile_df.copy().set_index("Month")
         row = new_df.loc[age['month'], new_df.columns[3:]]
 
         for key, value in row.items():
@@ -66,18 +85,46 @@ class BMIReference:
 
         return limits
 
+    def get_zscore(self, age, bmi):
+
+        """ The z-score will be provided to the AI for a more reliable evaluation. """
+
+        zscore_df = self.zscore_df.copy().set_index("Month")
+        z_columns = zscore_df.columns[3:]
+        row = zscore_df.loc[age['month'], z_columns]
+
+        for key, val in row.items():
+            if bmi >= val:
+                if key == "SD3neg":
+                    zscore = -3
+                elif key == "SD2neg":
+                    zscore = -2
+                elif key == "SD1neg":
+                    zscore = -1
+                elif key == "SD0":
+                    zscore = 0
+                elif key == "SD1":
+                    zscore = 1
+                elif key == "SD2":
+                    zscore = 2
+                elif key == "SD3":
+                    zscore = 3
+        return zscore
+
 
 class AIAnalysis:
 
+    """ Handles the AI interaction using the baby's input and calculated data. """
+
     @staticmethod
-    def analyze(df=None, bmi=None, gender=None, weight=None, height=None, age=None):
+    def analyze(df=None, bmi=None, gender=None, weight=None, height=None, age=None, z_score=0):
         client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
         try:
             prompt = client.models.generate_content(
                 model="gemini-3.8-flash",
-                contents=f"In 2-3 sentence, give a recommendation about a baby {gender} who"
+                contents=f"In 2-3 sentences, give a brief recommendation/evaluation about a baby {gender} who"
                          f"has a BMI of {bmi} at the age of {age} months"
-                         f"in terms of z-score and bmi category."
+                         f"in terms of z-score {z_score}."
             )
         except Exception as e:
             print(f"Exception error: {e}")
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
-from pprint import pprint
 
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, session
 import pandas as pd
 from dotenv import load_dotenv
 from flask_bootstrap import Bootstrap5
@@ -18,14 +17,20 @@ bootstrap = Bootstrap5(app)
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-BOY_DF = pd.read_csv("data/tab_bmi_boys_p_0_2.csv")
-GIRL_DF = pd.read_csv("data/tab_bmi_girls_p_0_2.csv")
+""" Excel files containing the WHO Data """
+BOY_PERCENTILE_DF = pd.read_csv("data/tab_bmi_boys_p_0_2.csv")
+BOY_ZSCORE_DF = pd.read_excel("data/bmi_boys_0-to-2-years_zcores.xlsx", engine="openpyxl")
+GIRL_PERCENTILE_DF = pd.read_csv("data/tab_bmi_girls_p_0_2.csv")
+GIRL_ZSCORE_DF = pd.read_excel("data/bmi_girls_0-to-2-years_zscores.xlsx", engine="openpyxl")
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    """ Created this form object just to render in the index.html """
+
+    """ Calculate the BMI based on the input data by the user. """
+
     form = UserInput()
+
     if form.validate_on_submit():
         age = Calculate.age(form.birthdate.data)
         gender = form.gender.data
@@ -34,22 +39,29 @@ def home():
         bmi = Calculate.bmi(weight, height)
 
         if gender == "boy":
-            bmi_ref = BMIReference(BOY_DF)
+            bmi_ref = BMIReference(BOY_PERCENTILE_DF, BOY_ZSCORE_DF)
         else:
-            bmi_ref = BMIReference(GIRL_DF)
+            bmi_ref = BMIReference(GIRL_PERCENTILE_DF, GIRL_ZSCORE_DF)
 
-        new_data = {"gender": gender, "age": age, "weight": weight, "height": height, "bmi": bmi,
-                    "percentile": bmi_ref.get_percentile(age, bmi)}
+        new_data = {"gender": gender, "age": age, "weight": weight, "height": height, "bmi": bmi}
 
+        new_data['percentile'] = bmi_ref.get_percentile(age=new_data['age'], bmi=new_data['bmi'])
+        new_data['zscore'] = bmi_ref.get_zscore(age=new_data['age'], bmi=new_data['bmi'])
+
+        """ This will call the Gemini model from the custom class to get evaluation of the BMI. """
         ai_output = AIAnalysis.analyze(
             bmi=new_data['bmi'],
-            age=new_data['age']['month']
+            age=new_data['age']['month'],
+            z_score=new_data['zscore']
         )
 
         new_data['ai_output'] = ai_output
 
+        """ Store the session data to avoid multiple calls to Gemini AI if already called once. """
         session['session_data'] = new_data
+
         return redirect(url_for('result'))
+
     return render_template("index.html", form=form)
 
 
@@ -58,15 +70,21 @@ def result():
 
     data = session.get("session_data")
 
+    """ 
+        Instead of passing the whole BMI dataframe to this router, 
+        Initialize another object bmi_ref to get a dataframe and plot the figure using create_figure()
+    """
+
     if data['gender'] == "boy":
-        bmi_ref = BMIReference(BOY_DF)
+        bmi_ref = BMIReference(BOY_PERCENTILE_DF, BOY_ZSCORE_DF)
     else:
-        bmi_ref = BMIReference(GIRL_DF)
+        bmi_ref = BMIReference(GIRL_PERCENTILE_DF, GIRL_ZSCORE_DF)
+    #
+    # data['percentile'] = bmi_ref.get_percentile(age=data['age'], bmi=data['bmi'])
+    # data['zscore'] = bmi_ref.get_zscore(age=data['age'], bmi=data['bmi'])
+    # pprint(data)
 
-    data['percentile'] = bmi_ref.get_percentile(data['age'], data['bmi'])
-    pprint(data)
-
-    fig = create_figure(data, bmi_ref.df)
+    fig = create_figure(data, bmi_ref.percentile_df)
 
     return render_template('result.html', new_data=data, fig=fig)
 
@@ -91,9 +109,14 @@ def create_figure(data, df):
         x="Month",
         y=data['percentile']
     )
+
     fig.add_scatter(
         x=[data['age']['month']],
-        y=[data['bmi']]
+        y=[data['bmi']],
+        name="Calculated BMI",
+        hovertemplate=(
+            "BMI: %{y:.2f} <br>"
+        )
     )
     fig.update_layout(
         yaxis_title="Percentile"
@@ -101,6 +124,21 @@ def create_figure(data, df):
     fig.layout.legend.title.text = "Percentiles"
     fig.update_xaxes(dtick=1)
     fig.update_yaxes(dtick=1)
+
+    """ Update the hover data of each line trace """
+    fig.for_each_trace(
+        lambda trace: trace.update(
+            hovertemplate=(
+                f"Percentile: {trace.name}<br>"
+                "Month: %{x}<br>"
+                "BMI: %{y:.2f}<extra></extra>"
+            )
+        ),
+        selector=lambda trace: trace.name.startswith("P")
+    )
+    fig.update_layout(
+        plot_bgcolor="#E3F5E1"
+    )
     fig_div = fig.to_html(full_html=False)
     return fig_div
 
